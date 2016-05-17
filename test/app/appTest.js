553a17200ff6e9748caf41817a3edd0badbbf4de
plusgut <github@webentwickler2-0.de>
2016-05-17 21:43:17 +0200

    [init] made throw test more readable

--- test/app/appTest.js
+++ test/app/appTest.js
@@ -1,7 +1,9 @@
 describe('tests of the root object', function() {
 	it('the framework should not be initialized', function() {
 		expect(snew.didInit).toEqual(false)
-		expect(snew.init.bind(snew)).toThrow('snew.viewHelper.entrance was not represented in dom properly null');
+		expect(function() {
+			snew.init();
+		}).toThrow('snew.viewHelper.entrance was not represented in dom properly null');
 		expect(snew.didInit).toEqual(false);
 	});
 });
