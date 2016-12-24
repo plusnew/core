dce2ba0e03329b5afce946aa0a857e85eebc6fa5
Carlo Jeske <jeske@sc-networks.com>
2016-12-24 14:28:50 +0100

    [config] added some validations

--- src/core/config.js
+++ src/core/config.js
@@ -1,29 +1,44 @@
 function Config() {}
 
-Config.prototype.set = function (config) {
-  if (this._config === undefined) {
-    this._config = config;
-  } else {
-    throw new Error('Config was already set');
-  }
+Config.prototype = {
+  set(config) {
+    if (this._config === undefined) {
+      this._config = config;
+    } else {
+      throw new Error('Config was already set');
+    }
 
-  return this._validate();
-};
+    return this._validate();
+  },
 
-Config.prototype.get = function (key) {
-  if (this._config === undefined) {
-    throw new Error('Config is not set yet');
-  } else {
-    return this._config[key];
-  }
-};
+  get(key) {
+    if (this._config === undefined) {
+      throw new Error('Config is not set yet');
+    } else {
+      return this._config[key];
+    }
+  },
 
-Config.prototype._validate = function () {
-  if (!this._config.components) {
-    throw new Error('The config has to have a components object');
-  }
+  _validate() {
+    return this._validateComponents()
+               ._validatePath();
+  },
 
-  return this;
+  _validateComponents() {
+    if (!this._config.components) {
+      throw new Error('The config has to have a components object');
+    }
+
+    return this;
+  },
+
+  _validatePath() {
+    if (!this._config.path) {
+      throw new Error('The config has to have a path-string');
+    }
+
+    return this;
+  },
 };
 
 export default Config;
