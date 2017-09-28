3a186c570ac9739e87c02b289c50474ed85fbad6
Carlo Jeske <jeske@sc-networks.com>
2017-09-28 10:11:55 +0200

    [plusnew] exported plusnew class, for better configurationability

--- index.ts
+++ index.ts
@@ -3,5 +3,5 @@ import component from './src/interfaces/component';
 import componentHandler from './src/interfaces/componentHandler';
 import './src/interfaces/jsx';
 
-export { component, componentHandler };
+export { component, componentHandler, Plusnew };
 export default new Plusnew();
