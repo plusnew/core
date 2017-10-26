662e5057a67bf9c91559486eaebd472f33ae8e11
plusgut <github@webentwickler2-0.de>
2017-10-26 12:01:00 +0200

    [tests] reenabled the tests

--- test/render/update.spec.tsx
+++ test/render/update.spec.tsx
@@ -61,65 +61,65 @@ describe('rendering the elements', () => {
   });
 
 
-  // it('does a value change with redchain with string to JSX.Element', () => {
-  //   const component = (lifeCycleHandler: LifeCycleHandler) => {
-  //     local.addOnChange(lifeCycleHandler.componentCheckUpdate);
-  //     return () => local.state === 'foo' ? local.state : <div>{local.state}</div>;
-  //   };
-  //   plusnew.render(component, container);
+  it('does a value change with redchain with string to JSX.Element', () => {
+    const component = (lifeCycleHandler: LifeCycleHandler) => {
+      local.addOnChange(lifeCycleHandler.componentCheckUpdate);
+      return () => local.state === 'foo' ? local.state : <div>{local.state}</div>;
+    };
+    plusnew.render(component, container);
 
-  //   expect(container.innerHTML).toBe('foo');
+    expect(container.innerHTML).toBe('foo');
 
-  //   local.dispatch('bar');
-  //   scheduler.clean();
+    local.dispatch('bar');
+    scheduler.clean();
 
-  //   expect(container.childNodes.length).toBe(1);
-  //   const target = container.childNodes[0] as HTMLElement;
-  //   expect(target.nodeName).toBe('DIV');
-  //   expect(target.innerHTML).toBe('bar');
-  // });
+    expect(container.childNodes.length).toBe(1);
+    const target = container.childNodes[0] as HTMLElement;
+    expect(target.nodeName).toBe('DIV');
+    expect(target.innerHTML).toBe('bar');
+  });
 
-  // it('does a value change with redchain with string to JSX.Element[]', () => {
-  //   const component = (lifeCycleHandler: LifeCycleHandler) => {
-  //     local.addOnChange(lifeCycleHandler.componentCheckUpdate);
-  //     return () => local.state === 'foo' ? local.state : [<div>{local.state}</div>, <span>{local.state}</span>];
-  //   };
-  //   plusnew.render(component, container);
+  it('does a value change with redchain with string to JSX.Element[]', () => {
+    const component = (lifeCycleHandler: LifeCycleHandler) => {
+      local.addOnChange(lifeCycleHandler.componentCheckUpdate);
+      return () => local.state === 'foo' ? local.state : [<div>{local.state}</div>, <span>{local.state}</span>];
+    };
+    plusnew.render(component, container);
 
-  //   expect(container.innerHTML).toBe('foo');
+    expect(container.innerHTML).toBe('foo');
 
-  //   local.dispatch('bar');
-  //   scheduler.clean();
+    local.dispatch('bar');
+    scheduler.clean();
 
-  //   expect(container.childNodes.length).toBe(2);
-  //   const target = container.childNodes[0] as HTMLElement;
-  //   expect(target.nodeName).toBe('DIV');
-  //   expect(target.innerHTML).toBe('bar');
+    expect(container.childNodes.length).toBe(2);
+    const target = container.childNodes[0] as HTMLElement;
+    expect(target.nodeName).toBe('DIV');
+    expect(target.innerHTML).toBe('bar');
 
-  //   const targetSecond = container.childNodes[1] as HTMLElement;
-  //   expect(targetSecond.nodeName).toBe('SPAN');
-  //   expect(targetSecond.innerHTML).toBe('bar');
-  // });
+    const targetSecond = container.childNodes[1] as HTMLElement;
+    expect(targetSecond.nodeName).toBe('SPAN');
+    expect(targetSecond.innerHTML).toBe('bar');
+  });
 
-  // it('does a value change with redchain with JSX.Element[] to string', () => {
-  //   const component = (lifeCycleHandler: LifeCycleHandler) => {
-  //     local.addOnChange(lifeCycleHandler.componentCheckUpdate);
-  //     return () => local.state === 'foo' ? [<div>{local.state}</div>, <span>{local.state}</span>] : local.state;
-  //   };
-  //   plusnew.render(component, container);
+  it('does a value change with redchain with JSX.Element[] to string', () => {
+    const component = (lifeCycleHandler: LifeCycleHandler) => {
+      local.addOnChange(lifeCycleHandler.componentCheckUpdate);
+      return () => local.state === 'foo' ? [<div>{local.state}</div>, <span>{local.state}</span>] : local.state;
+    };
+    plusnew.render(component, container);
 
-  //   expect(container.childNodes.length).toBe(2);
-  //   const target = container.childNodes[0] as HTMLElement;
-  //   expect(target.nodeName).toBe('DIV');
-  //   expect(target.innerHTML).toBe('foo');
+    expect(container.childNodes.length).toBe(2);
+    const target = container.childNodes[0] as HTMLElement;
+    expect(target.nodeName).toBe('DIV');
+    expect(target.innerHTML).toBe('foo');
 
-  //   const targetSecond = container.childNodes[1] as HTMLElement;
-  //   expect(targetSecond.nodeName).toBe('SPAN');
-  //   expect(targetSecond.innerHTML).toBe('foo');
+    const targetSecond = container.childNodes[1] as HTMLElement;
+    expect(targetSecond.nodeName).toBe('SPAN');
+    expect(targetSecond.innerHTML).toBe('foo');
 
-  //   local.dispatch('bar');
-  //   scheduler.clean();
+    local.dispatch('bar');
+    scheduler.clean();
 
-  //   expect(container.innerHTML).toBe('bar');
-  // });
+    expect(container.innerHTML).toBe('bar');
+  });
 });
