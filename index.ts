984ecd686f664127f258b86c6830427c50ea1683
plusgut <github@webentwickler2-0.de>
2017-11-29 19:55:17 +0100

    [component] added componentResult as a exported type

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@ import './src/interfaces/jsx';
 
 import Plusnew from './src';
 import scheduler from './src/scheduler';
-import component from './src/interfaces/component';
+import component, { componentResult } from './src/interfaces/component';
 import store from './src/store';
 
-export { Plusnew, scheduler, component, store };
+export { Plusnew, scheduler, component, componentResult, store };
 export default new Plusnew();
