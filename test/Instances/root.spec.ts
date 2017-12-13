0a6877109666eb107789dc1c48a8c60cdc838cb3
Carlo Jeske <jeske@sc-networks.com>
2017-12-13 17:04:45 +0100

    [root] added root errors

---
+++ test/Instances/root.spec.ts
@@ -0,0 +1,16 @@
+import Root from 'instances/types/Root/Instance';
+
+describe('Does the root-instance behave correctly', () => {
+  let root: Root;
+  beforeEach(() => {
+    root = new Root('foo', undefined, () => 0);
+  });
+  
+  it('getLength should throw exception', () => {
+    expect(() => root.getLength()).toThrow(new Error('getLength of RootElement is irrelevant'));
+  });
+
+  it('getLength should throw exception', () => {
+    expect(() => root.remove()).toThrow(new Error('The root element can\'t remove itself'));
+  });
+});
