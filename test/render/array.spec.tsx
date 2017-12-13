dd7efa0028853c756a39d39394f3a96c8701d0fc
Carlo Jeske <jeske@sc-networks.com>
2017-12-13 17:34:07 +0100

    [array] added item after array

--- test/render/array.spec.tsx
+++ test/render/array.spec.tsx
@@ -45,4 +45,41 @@ describe('rendering nested components', () => {
       expect((li as HTMLElement).innerHTML).toBe(list[index]);
     });
   });
+
+
+  it('does a initial list work, with pushing values', () => {
+    const list = ['first', 'second', 'third'];
+    const dependencies = {
+      local: new store((state: string[] | null, action: { type: string, payload?: string }) => {
+        if (state === null) {
+          return list;
+        } else if (action.payload) {
+          return [...state, action.payload];
+        }
+        return state;
+      }),
+    };
+
+    const MainComponent: component<{}, typeof dependencies> = () => {
+      return {
+        dependencies,
+        render: (props, { local }: typeof dependencies) => <ul>{local.state.map((value, index) => <li key={index}>{value}</li>)}<li>foo</li></ul>,
+      };
+    };
+
+    plusnew.render(MainComponent, container);
+
+    const ul = container.childNodes[0] as HTMLElement;
+    expect(container.childNodes.length).toBe(1);
+    expect(ul.tagName).toBe('UL');
+    expect(ul.childNodes.length).toBe(4);
+    ul.childNodes.forEach((li: Node, index) => {
+      expect((li as HTMLElement).tagName).toBe('LI');
+      if (index === 3) {
+        expect((li as HTMLElement).innerHTML).toBe('foo');
+      } else {
+        expect((li as HTMLElement).innerHTML).toBe(list[index]);
+      }
+    });
+  });
 });
