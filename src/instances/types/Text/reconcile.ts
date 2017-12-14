51d8a3ec5e2d98f99430ff8d2e78ea3f53898adf
Carlo Jeske <jeske@sc-networks.com>
2017-12-14 15:01:22 +0100

    [reconcile] removed unneeded check

--- src/instances/types/Text/reconcile.ts
+++ src/instances/types/Text/reconcile.ts
@@ -1,8 +1,6 @@
 import TextInstance from './Instance';
 
 export default function (newAbstractElement: string, instance: TextInstance) {
-  if (newAbstractElement !== instance.abstractElement) {
-    instance.setText(newAbstractElement);
-    instance.abstractElement = newAbstractElement;
-  }
+  instance.setText(newAbstractElement);
+  instance.abstractElement = newAbstractElement;
 }
