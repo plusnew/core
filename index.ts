81eae5dfd9bdedf483cbe78eff86b7e3c433808d
Carlo Jeske <jeske@sc-networks.com>
2018-01-17 16:03:38 +0100

    [events] added inputEvent type

--- index.ts
+++ index.ts
@@ -3,7 +3,8 @@ import './src/interfaces/jsx';
 import Plusnew from './src';
 import scheduler from './src/scheduler';
 import component, { componentResult } from './src/interfaces/component';
+import InputEvent from './src/interfaces/InputEvent';
 import store from 'redchain';
 
-export { Plusnew, scheduler, component, componentResult, store };
+export { Plusnew, scheduler, component, componentResult, store, InputEvent };
 export default new Plusnew();
