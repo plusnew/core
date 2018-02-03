de8fc4f879726b83a61f0ced0dd3aafb1563733e
plusgut <github@webentwickler2-0.de>
2018-02-03 11:50:19 +0100

    [reconciler] added isSameAbstractElement unittests

--- test/unit/reconciler/reconciler.spec.tsx
+++ test/unit/reconciler/reconciler.spec.tsx
@@ -1,5 +1,6 @@
 import Plusnew from 'index';
 import reconciler from 'instances/reconciler';
+import component from 'interfaces/component';
 
 describe('checking if reconciler works as expected', () => {
   describe('placeholder elements', () => {
@@ -8,26 +9,125 @@ describe('checking if reconciler works as expected', () => {
       plusnew = new Plusnew();
     });
 
-    it('placeholder same as array', () => {
-      expect(reconciler.isSameAbstractElement(false, false)).toBe(true);
-    });
+    describe('placeholder', () => {
+      it('placeholder same as placeholder', () => {
+        expect(reconciler.isSameAbstractElement(false, false)).toBe(true);
+      });
 
-    it('placeholder same as array', () => {
-      expect(reconciler.isSameAbstractElement(false, [])).toBe(false);
-    });
-    
-    it('placeholder same as array', () => {
-      expect(reconciler.isSameAbstractElement(false, <div />)).toBe(false);
-    });
+      it('placeholder same as array', () => {
+        expect(reconciler.isSameAbstractElement(false, [])).toBe(false);
+      });
+      
+      it('placeholder same as dom', () => {
+        expect(reconciler.isSameAbstractElement(false, <div />)).toBe(false);
+      });
 
-    it('placeholder same as array', () => {
-      const Component = () => {
-        return {
-          render: () => plusnew.createElement('div', null),
-          dependencies: {},
+      it('placeholder same as component', () => {
+        const Component = () => {
+          return {
+            render: () => <div />,
+            dependencies: {},
+          };
         };
-      };
-      expect(reconciler.isSameAbstractElement(false, <Component />)).toBe(false);
+        expect(reconciler.isSameAbstractElement(false, <Component />)).toBe(false);
+      });
+    });
+
+    describe('dom', () => {
+      it('same as placeholder', () => {
+        expect(reconciler.isSameAbstractElement(<div />, false)).toBe(false);
+      });
+
+      it('same as array', () => {
+        expect(reconciler.isSameAbstractElement(<div />, [])).toBe(false);
+      });
+      
+      it('same as dom, of equal type', () => {
+        expect(reconciler.isSameAbstractElement(<div />, <div />)).toBe(true);
+      });
+
+      it('same as dom, of unequal type', () => {
+        expect(reconciler.isSameAbstractElement(<div />, <span />)).toBe(false);
+      });
+
+      it('same as dom, of equal type, with same key', () => {
+        expect(reconciler.isSameAbstractElement(<div key="0"/>, <div key="0"/>)).toBe(true);
+      });
+
+      it('same as dom, of equal type, with different key', () => {
+        expect(reconciler.isSameAbstractElement(<div key="0"/>, <div key="1"/>)).toBe(false);
+      });
+
+      it('same as dom, of equal type, with one key', () => {
+        expect(reconciler.isSameAbstractElement(<div key="0"/>, <div/>)).toBe(false);
+      });
+
+
+      it('same as dom, of equal type, with one key', () => {
+        expect(reconciler.isSameAbstractElement(<div />, <div  key="0"/>)).toBe(false);
+      });
+
+      it('same as component', () => {
+        const Component: component = () => {
+          return {
+            render: () => <div />,
+            dependencies: {},
+          };
+        };
+        expect(reconciler.isSameAbstractElement(false, <Component />)).toBe(false);
+      });
+    });
+
+    describe('component', () => {
+      let Component: component;
+      let AnotherComponent: component;
+      beforeEach(() => {
+        Component = () => ({
+          render: () => <div />,
+          dependencies: {},
+        });
+
+        AnotherComponent = () => ({
+          render: () => <div />,
+          dependencies: {},
+        });
+      });
+
+      it('are components the same', () => {
+        expect(reconciler.isSameAbstractElement(<Component />, <Component />)).toBe(true);
+      });
+
+      it('are components the same with same key', () => {
+        expect(reconciler.isSameAbstractElement(<Component key={1}/>, <Component key={1}/>)).toBe(true);
+      });
+
+      it('are components the same with different key', () => {
+        expect(reconciler.isSameAbstractElement(<Component key={1}/>, <Component key={2}/>)).toBe(false);
+      });
+      
+      it('are different components the same', () => {
+        expect(reconciler.isSameAbstractElement(<Component />, <AnotherComponent />)).toBe(false);
+      });
+
+      it('are different components the same with same key', () => {
+        expect(reconciler.isSameAbstractElement(<Component key={1}/>, <AnotherComponent key={1}/>)).toBe(false);
+      });
+
+      it('are different components the same with different key', () => {
+        expect(reconciler.isSameAbstractElement(<Component key={1}/>, <AnotherComponent key={2}/>)).toBe(false);
+      });
+
+      it('are components the same as dom', () => {
+        expect(reconciler.isSameAbstractElement(<Component />, <div />)).toBe(false);
+      });
+
+      it('are components the same as placeholder', () => {
+        expect(reconciler.isSameAbstractElement(<Component />, false)).toBe(false);
+      });
+
+      it('are components the same as text', () => {
+        expect(reconciler.isSameAbstractElement(<Component />, 'text')).toBe(false);
+      });
     });
   });
 });
