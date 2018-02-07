688e0a10136d136fbcd84a8d140be207fbaa8fd2
Carlo Jeske <jeske@sc-networks.com>
2018-02-07 11:16:56 +0100

    [children] implemented children handling

--- src/interfaces/jsx.ts
+++ src/interfaces/jsx.ts
@@ -6,6 +6,9 @@ declare global {
      * the JSX.Element is a abstract representation of a Component
      */
     interface Element extends PlusnewAbstractElement { }
+
+    interface ElementChildrenAttribute { children: {}; }
+
     /**
      * All the DOM Nodes are here
      */
