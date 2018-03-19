d266e781adb9d1c45ebad0b552570315617d35ed
Carlo Jeske <jeske@sc-networks.com>
2018-03-19 14:58:25 +0100

    [placeholder] made it movable

--- test/unit/instances/placeholder.spec.ts
+++
@@ -1,9 +0,0 @@
-import Instance from 'instances/types/Placeholder/Instance';
-
-describe('placeholder', () => {
-  it('move', () => {
-    expect(() => {
-      Instance.prototype.move();
-    }).toThrow(new Error('Placeholder elements cant be moved'));
-  });
-});
