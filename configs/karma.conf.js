53e8ddb1cb42d09225e79733baab3f1cb51b72e1
Carlo Jeske <jeske@sc-networks.com>
2018-03-21 15:05:14 +0100

    [build] added cleandist directory and added remapping for tests

--- configs/karma.conf.js
+++ configs/karma.conf.js
@@ -20,7 +20,9 @@ module.exports = function (config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    // preprocessors: {},
+    preprocessors: {
+      'index.test.js': ['sourcemap'],
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
