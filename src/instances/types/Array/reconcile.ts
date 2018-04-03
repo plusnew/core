8d514f39c054fbca0e59e38a7e3f60487fb20685
Carlo Jeske <github@webentwickler2-0.de>
2018-04-03 09:13:21 +0200

    [arrayreconcile] fixed reconciling for non-key-based arrays (#50)

--- src/instances/types/Array/reconcile.ts
+++ src/instances/types/Array/reconcile.ts
@@ -16,22 +16,25 @@ function indexOf(instance: ArrayInstance, newAbstractElement: PlusnewAbstractEle
 
 export default function (newAbstractElements: PlusnewAbstractElement[], instance: ArrayInstance) {
 
-  // Checks old abstract elements, if they should get removed
-  // reason for that is, that we dont want to move new elements, because of old elements which get deleted
-  // moving causes animations to trigger, and that would be wrong in that case
-  for (let oldIndex = 0; oldIndex < instance.abstractElement.length; oldIndex += 1) {
-    let found = reconciler.isSameAbstractElement(instance.children[oldIndex].abstractElement, newAbstractElements[oldIndex]);
-    for (let newIndex = 0; newIndex < newAbstractElements.length && found === false; newIndex += 1) {
-      if (reconciler.isSameAbstractElement(instance.children[oldIndex].abstractElement, newAbstractElements[newIndex])) {
-        found = true;
+  // Removal of old-elements just works if key-property is existent
+  if (instance.abstractElement.length && instance.abstractElement[0].props && instance.abstractElement[0].props.key !== undefined) {
+    // Checks old abstract elements, if they should get removed
+    // reason for that is, that we dont want to move new elements, because of old elements which get deleted
+    // moving causes animations to trigger, and that would be wrong in that case
+    for (let oldIndex = 0; oldIndex < instance.abstractElement.length; oldIndex += 1) {
+      let found = reconciler.isSameAbstractElement(instance.children[oldIndex].abstractElement, newAbstractElements[oldIndex]);
+      for (let newIndex = 0; newIndex < newAbstractElements.length && found === false; newIndex += 1) {
+        if (reconciler.isSameAbstractElement(instance.children[oldIndex].abstractElement, newAbstractElements[newIndex])) {
+          found = true;
+        }
       }
-    }
 
-    if (!found) {
-      instance.children[oldIndex].remove();
-      instance.children.splice(oldIndex, 1);
-      instance.abstractElement.splice(oldIndex, 1);
-      oldIndex -= 1;
+      if (!found) {
+        instance.children[oldIndex].remove();
+        instance.children.splice(oldIndex, 1);
+        instance.abstractElement.splice(oldIndex, 1);
+        oldIndex -= 1;
+      }
     }
   }
 
@@ -63,6 +66,11 @@ export default function (newAbstractElements: PlusnewAbstractElement[], instance
       }
     }
   }
+
+  instance.children.splice(
+    newAbstractElements.length,
+    instance.abstractElement.length - newAbstractElements.length,
+  ).forEach(childInstance => childInstance.remove());
 
   instance.abstractElement = newAbstractElements;
 }
