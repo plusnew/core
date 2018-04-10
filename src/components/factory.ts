89083fca5de4c8e9c3039536a36279e16a630fd9
Carlo Jeske <jeske@sc-networks.com>
2018-04-10 17:08:27 +0200

    [types] changed depenencies

--- src/components/factory.ts
+++ src/components/factory.ts
@@ -1,20 +1,25 @@
 import ComponentInstance from '../instances/types/Component/Instance';
-import { deps, props, options } from '../interfaces/component';
+import { props, options } from '../interfaces/component';
+import { store } from 'redchain';
 
 export type result = plusnew.JSX.Element | null;
 export interface componentResult<componentProps extends Partial<props>> {
   (props: componentProps, instance: ComponentInstance): plusnew.JSX.Element | null;
 }
 
+export interface stores {
+  [key: string]: store<any, any>;
+}
+
 export interface factory {
-  <dependencies extends deps, componentProps extends Partial<props>>(
+  <dependencies extends stores, componentProps extends Partial<props>>(
     constructor: (props: componentProps) => dependencies,
     render: (props: componentProps, dependencies: dependencies) => result,
     options?: options<componentProps, dependencies>,
   ): (props: componentProps, instance: ComponentInstance) => result;
 }
 
-const factory: factory = <props extends Partial<props>, dependencies extends deps>(
+const factory: factory = <props extends Partial<props>, dependencies extends stores>(
   constructor: (props: props) => dependencies,
   render: (props: props, dependencies: dependencies) => result,
   options?: options<props, dependencies>,
