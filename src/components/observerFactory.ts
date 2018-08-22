038c69d9e72bfe7553f8ee87631191cd8b018e06
plusgut <github@webentwickler2-0.de>
2018-08-22 08:14:39 +0200

    [observer] made observer observe props

--- src/components/observerFactory.ts
+++ src/components/observerFactory.ts
@@ -12,10 +12,11 @@ export default function <state>(store: storeType<state, any>) {
   return class Observer extends AbstractClass<observerProps<state>> {
     instance: ComponentInstance<observerProps<state>>;
 
-    public render(_props: any, instance: ComponentInstance<observerProps<state>>) {
+    public render(props: any, instance: ComponentInstance<observerProps<state>>) {
       this.instance = instance;
 
       store.addOnChange(this.update);
+      props.getStore().addOnChange(this.update);
 
       return instance.props.render(store.getCurrentState());
     }
@@ -24,22 +25,39 @@ export default function <state>(store: storeType<state, any>) {
      * has to be a property on the componentinstance, to create new reference each time
      * so that removal of correct listener is possible
      */
-    private update = (state: state) => {
+    private update = () => {
       this.instance.render(
         this.instance.props.render(store.getCurrentState()),
       );
     }
 
+    /**
+     * unregisters the event
+     */
     public componentWillUnmount() {
       store.removeOnChange(this.update);
     }
 
+    /**
+     * this component should event be created in shallow mode
+     */
     static shouldCreateComponent() {
       return true;
     }
 
+    /**
+     * returns the current state of the store
+     * if you want to have the state when it changes, better observe the store
+     */
     static getCurrentState() {
       return store.getCurrentState();
+    }
+
+    /**
+     * gives the corresponding store to this observer
+     */
+    static getStore() {
+      return store;
     }
   };
 }
